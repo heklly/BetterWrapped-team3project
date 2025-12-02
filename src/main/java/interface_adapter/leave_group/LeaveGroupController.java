969c75dfb0d4d0ac4f90e7d80c841238038bcad5
samiwal1a <s.walia@mail.utoronto.ca>
2025-12-02 02:09:59 -0500

    cleaned up some things in LeaveGroupController

--- src/main/java/interface_adapter/leave_group/LeaveGroupController.java
+++ src/main/java/interface_adapter/leave_group/LeaveGroupController.java
@@ -1,6 +1,9 @@
 package interface_adapter.leave_group;
 
+import entity.Group;
+import entity.SpotifyUser;
 import use_case.leave_group.LeaveGroupInputBoundary;
+import use_case.leave_group.LeaveGroupInputData;
 
 /**
  * The controller for the Leave Group Use Case.
@@ -8,16 +11,17 @@ import use_case.leave_group.LeaveGroupInputBoundary;
 
 public class LeaveGroupController {
 
-    private LeaveGroupInputBoundary leaveGroupUseCaseInteractor;
+    private final LeaveGroupInputBoundary leaveGroupUseCaseInteractor;
 
     public LeaveGroupController(LeaveGroupInputBoundary leaveGroupUseCaseInteractor) {
-        this.leaveGroupUseCaseInteractor = this.leaveGroupUseCaseInteractor;
+        this.leaveGroupUseCaseInteractor = leaveGroupUseCaseInteractor;
     }
 
     /**
      * Executes the Leave Group Use Case.
      */
-    public void execute() {
-        leaveGroupUseCaseInteractor.execute();
+    public void execute(SpotifyUser user, Group group) {
+        LeaveGroupInputData inputData = new LeaveGroupInputData(user, group);
+        leaveGroupUseCaseInteractor.execute(inputData);
     }
 }
