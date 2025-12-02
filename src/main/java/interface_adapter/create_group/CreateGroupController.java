baec3db56b62d45eab6ba9ac9514953f64dff004
samiwal1a <s.walia@mail.utoronto.ca>
2025-12-02 02:05:58 -0500

    trying to clean up some things in input data (adding getusers, deleting owner, we are no longer adding owner to group)

--- src/main/java/interface_adapter/create_group/CreateGroupController.java
+++ src/main/java/interface_adapter/create_group/CreateGroupController.java
@@ -24,12 +24,12 @@ public class CreateGroupController {
     /**
      * Creates a group using the provided user input.
      *
-     * @param group_name the name of the group
-     * @param owner the user creating the group
-     * @param initialMembers  list of initial members
+     * @param group_name     the name of the group
+     * @param spotifyUser
+     * @param initialMembers list of initial members
      */
-    public void execute(String group_name, SpotifyUser owner, List<SpotifyUser> initialMembers) {
-        CreateGroupInputData inputData = new CreateGroupInputData(group_name, owner, initialMembers);
+    public void execute(String group_name, SpotifyUser spotifyUser, List<SpotifyUser> initialMembers) {
+        CreateGroupInputData inputData = new CreateGroupInputData(group_name, initialMembers);
         createGroupUseCase.execute(inputData);
 
     }
